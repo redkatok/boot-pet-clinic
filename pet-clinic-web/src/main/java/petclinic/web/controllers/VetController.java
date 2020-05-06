1f54841bee08f10f2eb71916ff64f6375c46293e
mikekatok96@gmail.com <Megatron9260>
2020-05-06 22:09:12 +0300

    add vet index page and controller

---
+++ pet-clinic-web/src/main/java/petclinic/web/controllers/VetController.java
@@ -0,0 +1,15 @@
+package petclinic.web.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets","/vets/index"})
+    public String listVets(){
+        return "/vets/index";
+    }
+}
