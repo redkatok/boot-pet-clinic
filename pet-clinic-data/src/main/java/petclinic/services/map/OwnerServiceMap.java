9c84659e2e00620518c677d71667850da7475c08
mikekatok96@gmail.com <Megatron9260>
2020-05-09 00:02:13 +0300

    add bootstrap data by commandliner

--- pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package petclinic.services.map;
 
 import petclinic.model.Owner;
-import petclinic.services.CrudService;
+import petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
     @Override
     public Set<Owner> findAll() {
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(Long id) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package petclinic.services.map;
 
 import petclinic.model.Pet;
-import petclinic.services.CrudService;
+import petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
